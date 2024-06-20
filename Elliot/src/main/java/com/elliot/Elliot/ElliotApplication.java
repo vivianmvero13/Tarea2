d4c8f7ed175e1d1547da10d01605ad6b704cc2e6
vivianmvero13 <165861899+vivianmvero13@users.noreply.github.com>
2024-06-19 23:57:03 -0600

    Agregando cambios

---
+++ Elliot/src/main/java/com/elliot/Elliot/ElliotApplication.java
@@ -0,0 +1,13 @@
+package com.elliot.Elliot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElliotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElliotApplication.class, args);
+	}
+
+}
---
+++ Elliot/src/test/java/com/elliot/Elliot/ElliotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliot.Elliot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElliotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
